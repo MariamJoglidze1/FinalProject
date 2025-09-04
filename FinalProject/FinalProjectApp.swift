53d57660e65d12cdfd543bd3c53c48ee06c13241
Mariami <mariamjoglidze1@gmail.com>
2025-09-04 19:56:27 +0400

    first commit

---
+++ FinalProject/FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectApp.swift
+//  FinalProject
+//
+//  Created by Mariam Joglidze on 04.09.25.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
