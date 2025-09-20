ad4b29021eb5b28c6ac7eb25986411e8eeb91e3e
MariamJoglidze1 <mariam_joglidze@epam.com>
2025-09-20 17:34:14 +0400

    Created LocalManaged, saved language if exists

---
+++ FinalProject/Services and Managers/LocalManager.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+import Combine
+
+class LocaleManager: ObservableObject {
+    @Published var locale: Locale = .current
+    
+    func setLanguage(_ identifier: String) {
+        locale = Locale(identifier: identifier)
+        UserDefaults.standard.set(identifier, forKey: "AppLanguage")
+    }
+    
+    init() {
+        if let savedLang = UserDefaults.standard.string(forKey: "AppLanguage") {
+            locale = Locale(identifier: savedLang)
+        }
+    }
+}
