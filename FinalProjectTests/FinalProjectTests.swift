de032446bfc182317dfb6ca99f9112f0c8d1d2d6
MariamJoglidze1 <mariam_joglidze@epam.com>
2025-09-24 18:31:30 +0400

    Added  Swift Test in Project

---
+++ FinalProjectTests/FinalProjectTests.swift
@@ -0,0 +1,16 @@
+//
+//  FinalProjectTests.swift
+//  FinalProjectTests
+//
+//  Created by Mariam Joglidze on 24.09.25.
+//
+
+import Testing
+
+struct FinalProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
